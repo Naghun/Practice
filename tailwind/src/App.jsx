a6d276842cbeab68351546ba98e6819ecba4404c
Abdullah Sinanović <budad-97@hotmail.com>
2024-06-10 23:13:29 +0200

    small tailwind height and width exercises

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
       <h3 className='text-red-700 bg-white'>This one is reddish</h3>
       <h5 className='text-yellow-300 bg-blue-300'>barely seen</h5>
       <h5 className='text-mycol bg-blue-300'>barely seen</h5>
+      <p className='leading-normal capitalize truncate'>I ja sam lud sto sam uopste pokusavao</p>
+      <p className='bg-red-700 p-5 pr-10 mr-4 text-white font-bold'>Klase za padding</p>
+      <div className='min-w-0.5 bg-green-300'>ovdje nesto</div>
     </>
   )
 }
